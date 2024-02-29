1c13dcaf24c08c405ec6822a2ab1d15b84af6d7e
guptasneha12 <guptasneha2003.sg@gmail.com>
2024-02-29 18:32:59 +0530

    my-commit

---
+++ src/components/index.jsx
@@ -0,0 +1,8 @@
+import { Input } from "./Input";
+import { Text } from "./Text";
+import { Heading } from "./Heading";
+import { RatingBar } from "./RatingBar";
+import { Button } from "./Button";
+import { Img } from "./Img";
+import { SelectBox } from "./SelectBox";
+export { Input, Text, Heading, RatingBar, Button, Img, SelectBox };
